8dcdf840e14202297b4a89b5803e9ba170b928e8
Mattis Faucheux <mattis.faucheux@gmail.com>
2019-12-23 15:35:10 -0500

    First Person Controller
    mouvement, jump & sprint

---
+++ WinterProject/Assets/Scripts/PlayerMouvement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouvement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float playerSpeed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    public float sprintSpeed = 2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public KeyCode sprint;
+   
+    Vector3 velocity;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKey(sprint))
+        {
+            controller.Move(move * playerSpeed * Time.deltaTime * sprintSpeed);
+        }
+        else
+        {
+            controller.Move(move * playerSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
