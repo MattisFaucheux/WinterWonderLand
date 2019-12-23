fe61a24023a35ea277c3a3c5af87a80fe38e7a22
Mattis Faucheux <mattis.faucheux@gmail.com>
2019-12-23 18:56:17 -0500

    Add shoot rate, impact effect & flash effect

--- WinterProject/Assets/Scripts/gun.cs
+++ WinterProject/Assets/Scripts/gun.cs
@@ -5,15 +5,22 @@ public class gun : MonoBehaviour
 
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
 
     public Camera playerCam;
 
-   // public ParticleSystem muzzleFlash;
+    public GameObject flash;
+    public GameObject muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
                
@@ -22,19 +29,33 @@ public class gun : MonoBehaviour
     void Shoot()
     {
 
-      //The flash when we shoot
-      //  muzzleFlash.Play();
+        //The flash when we shoot
+        // muzzleFlash.Play();
+
+        GameObject flashGO = Instantiate(muzzleFlash, flash.transform.position, Quaternion.LookRotation(flash.transform.localPosition));
+        flashGO.GetComponent<ParticleSystem>().Play();
+        Destroy(flashGO, 0.1f);
 
         RaycastHit hit;
         if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
-
-           Target target = hit.transform.GetComponent<Target>();
+            Target target = hit.transform.GetComponent<Target>();
             if(target != null)
             {
                 target.TakeDamage(damage);
             }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+
+
+            //Impact effect
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            Destroy(impactGO, 0.5f);
         }
     }
 }
