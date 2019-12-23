2183101eb0b5a19d8939efef8a16fa3cb9b013e2
RENCUREL <pierre.rencurel@orange.fr>
2019-12-23 18:05:00 -0500

    Initial transposition of state machine

---
+++ WinterProject/Assets/Scripts/FSM/State.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines a behaviour routine
+/// </summary>
+[System.Serializable]
+public abstract class State : ScriptableObject
+{
+    /// <summary>
+    /// The reference to the original state
+    /// </summary>
+    [System.NonSerialized]
+    public State initialState;
+    /// <summary>
+    /// The state machine this instance belongs to
+    /// </summary>
+    [System.NonSerialized]
+    public StateMachine stateMachine;
+
+    /// <summary>
+    /// Called when the state machine enters this state. Use it for initialization.
+    /// </summary>
+    public virtual void Enter() { }
+
+    /// <summary>
+    /// Called every frame while the state machine is in this state. This is the routine.
+    /// </summary>
+    public virtual void Update() { }
+
+    /// <summary>
+    /// Called when leaving this state
+    /// </summary>
+    public virtual void Exit() { }
+
+    /// <summary>
+    /// Called when the state machine is disabled
+    /// </summary>
+    public virtual void OnStateMachineDisable() { }
+
+    public void ExitToTransition(int index)
+    {
+        StateMachine.Transition transition = GetTransition(index); // Get the transition at index
+        stateMachine.g_activeState = null; // Stop the current state machine
+        if (transition != null)
+            transition.stateMachine.EnterState(transition.state); // the destination state machine enters the destination state
+    }
+
+    public void ExitToFirstTransition()
+    {
+        ExitToTransition(0);
+    }
+
+    public StateMachine.Transition GetTransition(int index)
+    {
+        StateMachine.StatePair pair = stateMachine.GetPairFromState(initialState); // The State/Transitions pair of this state
+        if (pair.transitions.Count <= index) return null; // In case there is no transitions, skip
+        StateMachine.Transition transition = pair.transitions[index]; // Get the first transition
+        return transition;
+    }
+}
---
+++ WinterProject/Assets/Scripts/FSM/StateMachine.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Define state initialisation and manage state allocation.
+/// </summary>
+public class StateMachine : MonoBehaviour
+{
+    [System.Serializable]
+    public class Transition
+    {
+        public StateMachine stateMachine;
+        public State state;
+    }
+
+    [System.Serializable]
+    public class StatePair
+    {
+        public State state;
+        public List<Transition> transitions = new List<Transition>();
+    }
+
+    public List<StatePair> allStates;
+    public State startingState;
+    public State g_activeState;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Start()
+    {
+        EnterState(startingState);
+    }
+
+    void Init()
+    {
+        var StateToCopy = new Dictionary<State, State>();
+
+        foreach (StatePair pair in allStates)
+        {
+            if (!StateToCopy.ContainsKey(pair.state))
+            {
+                //Contains instance of state
+                var m_temp = Instantiate(pair.state);
+                m_temp.initialState = pair.state;
+                m_temp.stateMachine = this;
+
+                //created instance of state 
+                StateToCopy.Add(pair.state, m_temp);
+            }
+
+            pair.state = StateToCopy[pair.state];
+        }
+    }
+
+    public StatePair GetPairFromState(State state)
+    {
+        foreach (StatePair pair in allStates)
+        {
+            if (pair.state.initialState == state)
+            {
+                return pair;
+            }
+        }
+        return null;
+    }
+
+    public void EnterState(State state)
+    {
+        foreach (StatePair pair in allStates)
+        {
+            if (pair.state.initialState == state)
+            {
+                pair.state.Enter();
+                g_activeState = pair.state;
+                return;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (g_activeState != null)
+        {
+            g_activeState.Update();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (g_activeState != null) g_activeState.OnStateMachineDisable();
+        g_activeState = null;
+    }
+
+    private void OnEnable()
+    {
+        if (g_activeState == null) EnterState(startingState);
+    }
+}
