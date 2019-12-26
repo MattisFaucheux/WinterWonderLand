ce9cf92ca4242a9017c6b8bd5a8f4db54face2bb
RENCUREL <pierre.rencurel@orange.fr>
2019-12-26 00:03:52 -0500

    Starting to create roaming state and behaviour

---
+++ WinterProject/Assets/Scripts/Enemies/Behaviours/Behaviour_Roam.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Behaviour_Roam : MonoBehaviour
+{
+    /// <summary>
+    /// The AI agent
+    /// </summary>
+    NavMeshAgent g_agent;
+
+    /// <summary>
+    /// Referential used to determine roaming sector
+    /// </summary>
+    Transform g_anchor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WinterProject/Assets/Scripts/Enemies/State_Roam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "State Roam", menuName = "State Machine/States/Roam")]
+public class State_Roam : State
+{
+
+    public override void Enter()
+    {
+        stateMachine.GetComponent<Behaviour_Roam>().enabled = true;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        ExitToFirstTransition();
+    }
+}
