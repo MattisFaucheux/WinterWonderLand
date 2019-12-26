5a461432d30251d63c3f86b0c50ab90a26888e4b
RENCUREL <pierre.rencurel@orange.fr>
2019-12-26 16:20:53 -0500

    Instantiate anchor for roaming

---
+++ WinterProject/Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject g_anchorInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(g_anchorInstance, gameObject.transform.position, Quaternion.identity, this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
