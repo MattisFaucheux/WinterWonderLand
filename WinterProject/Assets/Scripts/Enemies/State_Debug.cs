069f201a8e52e9d3017963a35b7a2e53c3bb09d0
RENCUREL <pierre.rencurel@orange.fr>
2019-12-25 23:48:35 -0500

    State machine works with one agent

--- WinterProject/Assets/Scripts/Enemies/State_Debug.cs
+++ WinterProject/Assets/Scripts/Enemies/State_Debug.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Manages Idle state behaviour
 [System.Serializable]
-[CreateAssetMenu(fileName = "State Idle", menuName = "State Machine/States/Idle")]
-public class State_Idle : State
+[CreateAssetMenu(fileName = "State Debug", menuName = "State Machine/States/Debug")]
+public class State_Debug : State
 {
 
     public float g_duration;
     private float g_timer;
+    public string message;
 
     public override void Enter()
     {
         g_timer = g_duration;
+        Debug.Log("State Machine Debug > " + message);
     }
 
     public override void Update()
